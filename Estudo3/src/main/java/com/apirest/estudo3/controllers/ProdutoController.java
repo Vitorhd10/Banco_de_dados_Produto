d262b60a2d8e45fae08bb72f92648588b857ceae
Vitorhd10 <87782162+Vitorhd10@users.noreply.github.com>
2021-08-31 15:45:12 -0300

    update do projeto

--- Estudo3/src/main/java/com/apirest/estudo3/controllers/ProdutoController.java
+++ Estudo3/src/main/java/com/apirest/estudo3/controllers/ProdutoController.java
@@ -10,12 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 import static org.springframework.data.jpa.domain.AbstractPersistable_.ID;
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 @RestController
 public class ProdutoController {
 
@@ -32,18 +33,28 @@ public class ProdutoController {
         return produtoService.listaProdutounico(id);
     }
 
+    @PostMapping(value = "/salvar")
+    public ModelAndView salvar(@ModelAttribute Produto produto) {
+        ModelAndView mv = new ModelAndView("salvo");
+        Produto produto1 = produtoService.salvar(produto);
+        return mv;
+    }
+
     @GetMapping("/salvo")
-    public String showProdutoList(Model model) {
-        model.addAttribute("produto");
+    public String salvo(Model model) {
         return "salvo";
     }
 
-    @PostMapping(value = "/sucesso")
-    public String salvar(@ModelAttribute Produto produto, BindingResult errors, Model model) {
+    @GetMapping(value = "/index")
+    public ModelAndView newProduto() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
 
-        Produto produto1 = produtoService.salvar(produto);
-        return "redirect:/salvo";
-
+    @GetMapping("/")
+    public ModelAndView formIndex(Model model) {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
     }
 
 
