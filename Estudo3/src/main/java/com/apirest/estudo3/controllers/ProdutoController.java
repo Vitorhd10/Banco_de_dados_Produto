34679fb2981f785fbf2bc2cec427607289b86af7
Vitorhd10 <87782162+Vitorhd10@users.noreply.github.com>
2021-09-01 23:33:30 -0300

    controller atualizado

--- Estudo3/src/main/java/com/apirest/estudo3/controllers/ProdutoController.java
+++ Estudo3/src/main/java/com/apirest/estudo3/controllers/ProdutoController.java
@@ -1,42 +1,36 @@
 package com.apirest.estudo3.controllers;
 
-
 import com.apirest.estudo3.models.Produto;
-import com.apirest.estudo3.repository.ProdutoRepository;
 import com.apirest.estudo3.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.ID;
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @RestController
 public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
 
-    @GetMapping("/produtos")
-    public List<Produto> listaProdutos() {
-        return produtoService.listaProdutos();
+    @GetMapping("/salvar")
+    private ModelAndView listProduto(@ModelAttribute Produto produto) {
+        ModelAndView mv = new ModelAndView("salvo");
+        List<Produto> produtos = this.produtoService.listaProdutos();
+        mv.addObject("produto", produtos);
+        return mv;
     }
 
-    @GetMapping("/produto/{id}")
-    public Produto listaProdutounico(@PathVariable(value = "id") Long id) {
-        return produtoService.listaProdutounico(id);
-    }
 
     @PostMapping(value = "/salvar")
     public ModelAndView salvar(@ModelAttribute Produto produto) {
         ModelAndView mv = new ModelAndView("salvo");
         Produto produto1 = produtoService.salvar(produto);
+        List<Produto> produtos = this.produtoService.listaProdutos();
+        mv.addObject("produto", produtos);
         return mv;
     }
 
